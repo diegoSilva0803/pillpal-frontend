7b60290786c7a2a7e30dab19a619645eaf45e4e1
die-go-dev <diego.rodrigues0308@gmail.com>
2024-04-13 09:20:00 -0700

    timer fixed

--- src/components/MedFormTable/MedFormTable.jsx
+++ src/components/MedFormTable/MedFormTable.jsx
@@ -2,15 +2,7 @@ import "./MedFormTable.scss";
 import "../../pages/User/User.scss";
 import { IoIosClose } from "react-icons/io";
 import { useState, useEffect } from "react";
-
-const timeOptions = [];
-for (let hour = 0; hour < 24; hour++) {
-  const hourStr = hour.toString().padStart(2, "0");
-  timeOptions.push({
-    value: `${hourStr}:00`,
-    label: `${hourStr}:00 ${hour < 12 ? "AM" : "PM"}`,
-  });
-}
+import { Preview } from "@mui/icons-material";
 
 export default function MedFormTable({
   handleSave,
@@ -21,38 +13,64 @@ export default function MedFormTable({
 }) {
   const [numTimes, setNumTimes] = useState([]);
   const [numTimeInputFields, setNumTimeInputFields] = useState([]);
-  console.log(numTimeInputFields);
-  console.log(numTimeInputFields[0]);
+  //   console.log(numTimeInputFields);
+  //   console.log(numTimeInputFields[0]);
 
   const onChangeTimes = (event) => {
-
-    const myArr = [];
-    for (let x = 0; x < event.target.value || 0; x++) {
-      myArr.push(x);
+    try {
+      const myArr = [];
+      for (let x = 0; x < event.target.value || 0; x++) {
+        myArr.push(x);
+      }
+      if (event.target.value > 0 && event.target.value < 11) {
+        setNumTimes(myArr || []);
+      }
+      handleOnChange(event);
+    } catch (error) {
+      alert("DOSAGE CANNOT BE MORE THAN 10!");
     }
-    setNumTimes(myArr || []);
-    handleOnChange(event);
   };
 
   useEffect(() => {
+    const handleOnChangeInputTime = (e) => {
+    //   console.log(e.target.id);
+      const dosageTime = {
+        id: e.target.id,
+        value: e.target.value,
+      };
+      if (numTimeInputFields.length < formData.dosageTimes.length) {
+        while (numTimeInputFields.length < formData.dosageTimes.length) {
+          formData.dosageTimes.pop();
+        }
+      }
+      const doesDosageExist = formData.dosageTimes.findIndex(
+        (dose) => dose.id === dosageTime.id
+      );
+    //   console.log("doesdosageexist", doesDosageExist);
+      if (doesDosageExist !== -1) {
+        formData.dosageTimes[doesDosageExist] = dosageTime;
+      } else {
+        formData.dosageTimes.push(dosageTime);
+      }
+      console.log(e.target.value);
+      // setFormData({...formData, })
+      // console.log(e);
+      handleOnChange(e);
+    //   console.log(e.target.value);
+    };
     setNumTimeInputFields(
       numTimes.map((dosageTime, index) => {
-        console.log(dosageTime);
         return (
           <div key={index}>
-            <select
-              className=""
-              name={`dosageTimes[${dosageTime}]`} // Update name based on index
-              defaultValue={formData.dosageTimes[dosageTime] || ""}
-              onChange={handleOnChange}
-            >
-              <option value="">Select Time</option>
-              {timeOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
+            <input
+              type="time"
+              id={dosageTime}
+              required
+              onChange={(e) => {
+                handleOnChangeInputTime(e);
+                // handleOnChange(e);
+              }}
+            />
           </div>
         );
       })
@@ -79,7 +97,7 @@ export default function MedFormTable({
         <input
           type="number"
           min="0"
-          max="6"
+          max="10"
           minLength={0}
           maxLength={6}
           id="freq"
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,10 @@
 import "./Home.scss";
 import { Link } from "react-router-dom";
-import backgroundImg from "../../images/pillpal-bgImg.jpg";
 
 function Home() {
   return (
     <div className="home">
-      <div
-        className="headerContainer"
-        style={{ backgroundImage: `url(${backgroundImg})` }}
-      >
+      <div className="headerContainer">
         <h1 className="app-name">PILLPAL</h1>
         <p>YOUR HEALTHY REMINDER</p>
         <Link to="/user">
